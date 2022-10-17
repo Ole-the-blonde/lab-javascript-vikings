b1dd8ac4e815719c514aa424c9ddc6b8429c200d
Ole Demay <olesyademy@gmail.com>
2022-10-18 00:39:27 +0200

    started

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,26 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+  }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    super(attack);
+  }
+}
 
 // Saxon
 class Saxon {}
