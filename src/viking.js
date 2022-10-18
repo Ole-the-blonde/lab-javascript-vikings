01425c09736a047606eeb46c67a6ec4daf54758f
Ole Demay <olesyademy@gmail.com>
2022-10-18 12:41:10 +0200

    finished

--- src/viking.js
+++ src/viking.js
@@ -14,16 +14,50 @@ class Soldier {
 }
 // Viking
 class Viking extends Soldier {
-  constructor(name) {
+  constructor(name, health, strenth) {
+    super(health, strenth);
     this.name = name;
-    this.health = health;
-    this.strength = strength;
-    super(attack);
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `${this.name} has received ${theDamage} points of damage`;
+    }
+
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
   }
 }
-
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamage} points of damage`;
+    }
 
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 // War
-class War {}
+class War {
+  //   constructor(vikingArmy, saxonArmy) {
+  //     this.vikingArmy = vikingArmy;
+  //     this.saxonArmy = saxonArmy;
+  //   }
+  addViking() {}
+  addSaxon() {}
+  vikingAttack() {}
+  saxonAttack() {}
+  showStatus() {}
+}
